2d907af104241e3317f4f663c20403130e85f02f
skandergod <thedanidacosta@gmail.com>
2024-10-07 12:03:04 -0400

    generating an empty sample file, updated installing guide, updated .gitignore

--- Script.py
+++ Script.py
@@ -1,7 +1,16 @@
 #Pre-requisites
 #install openpyxl
 #pip install openpyxl
+#or
+#py -m pip install openpyxl
+#virtual enviorments can mess up with python modules, 2nd one works with pyenv
 import openpyxl
 from openpyxl import Workbook, load_workbook
+
+wb = Workbook()
+
+ws = wb.active
+
+wb.save("sample.xlsx")
 
 print("This line will be printed.") 
